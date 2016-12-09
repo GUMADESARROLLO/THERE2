6d2e584da7eb782359d034a2dff46c234e085369
luis.perez <analista.guma@gmail.com>
2016-12-09 14:13:52 -0600

    Se eliminaron los objetos y clases no usadas. Elimine uso de variable innecesaria.

--- core/src/main/java/com/desarrollo/guma/core/Usuario.java
+++ core/src/main/java/com/desarrollo/guma/core/Usuario.java
@@ -67,10 +67,10 @@ public class Usuario {
         try
         {
             String xxx = "";
-            xxx = encriptarEnMD5(PASSWORD);
+            //xxx = encriptarEnMD5(PASSWORD);
             myDbHelper = new SQLiteHelper(basedir, context);
             myDataBase = myDbHelper.getReadableDatabase();
-            Cursor cursor = myDataBase.rawQuery("select * from Usuarios where Credencial='"+Usuario+"' and PASSWORD='"+xxx+"' ", null);
+            Cursor cursor = myDataBase.rawQuery("select * from Usuarios where Credencial='"+Usuario+"' and PASSWORD='"+encriptarEnMD5(PASSWORD)+"' ", null);
             if(cursor.getCount() > 0)
             {
                 Correcto = true;
